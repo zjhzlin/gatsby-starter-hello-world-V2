f643270ee804bd4fb6d17a0e7cdcd36be6299cce
zjhzlin <smilynnzhang@gmail.com>
2022-06-18 09:32:29 +0100

    first trial

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from "react"
+import Layout from "./components/Layout"
+
+export default function Home() {
+  return (
+    <Layout>
+      <h1>
+        Lynn's Portfolio
+      </h1>
+    </Layout>
+
+  )
+}
